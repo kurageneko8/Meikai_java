c8a341f04fcf0aba6db07b6d8d2f094bdcc22cd7
ryo <ryota.ddd@gmail.com>
2020-01-01 20:24:31 +0900

    started to learn pointer

---
+++ Chap06/ex2.java
@@ -0,0 +1,20 @@
+// package MeikaiJava.Chap06;
+
+/**
+ * ex2
+ */
+public class ex2 {
+    public static void main(String[] args) {
+     int[] a = new int[5];
+     
+    //  int min = 0;
+    int v;
+     for(int i = 0; i < a.length; i++)
+        v = a.length - i; 
+        // a[i] = v;
+        System.out.println("a.length: "+ a.length);
+        // System.out.print("a[" + min + "] = " + a[min]);
+        // ++min;
+    }
+    
+}
