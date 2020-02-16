bfdb18e6b53cf6fc9b813f23dcb8ae397690715b
ryo <ryo@ryonoMacBook-ea.local>
2020-02-16 13:22:46 +0900

    restarted chapter08

---
+++ chap08/car2/Car.java
@@ -0,0 +1,15 @@
+/**
+ * Car
+ */
+public class Car {
+
+    private String name;
+    private int width;
+    private int height;
+    private int length;
+    private double x;
+    private double y;
+    private double z;
+    private Day purchaseDay;
+
+}
